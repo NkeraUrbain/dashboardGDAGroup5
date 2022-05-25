a8640b2e018fb248d4aff7bcba590e0039c37365
Doddy MATABARO <matabarododdy@gmail.com>
2022-05-25 11:31:49 +0200

    feat: ajout du side bar avec tout les elements

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Roboto">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@6.5.95/css/materialdesignicons.min.css">
           <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
@@ -13,7 +14,27 @@
     <div class="container">
       <nav>NavBar</nav>
       <main>Main</main>
-      <div id="sidebar">Sidebar</div>
+      <div id="sidebar">
+        
+      
+      
+      <ul class="side-content1">
+        <li class='titre'><span class="mdi mdi-view-dashboard" style="font-size: 30px; ">  Dashboad</span></li>
+        <li class=''><span class="mdi mdi-home" style="font-size: 20px; ">  Home</span></li>
+        <li class=''><span class="mdi mdi-card-account-details" style="font-size: 20px; ">  Profile</span></li>
+        <li class=''><span class="mdi mdi-message-reply-outline" style="font-size: 20px; color: white;">  Messages</span></li>
+        <li class=''><span class="mdi mdi-clock-time-three" style="font-size: 20px; ">  History</span></li>
+        <li class=''><span class="mdi mdi-file-document-multiple" style="font-size: 20px; ">  Tasks</span></li>
+        <li class=''><span class="mdi mdi-account-group" style="font-size: 20px; ">  communauties</span></li>
+      </ul>
+        <ul class="side-content2">
+                <li class=''><span class="mdi mdi-cog" style="font-size: 20px; ">  Settings</span></li>
+        <li class=''><span class="mdi mdi-help" style="font-size: 20px; ">  Support</span></li>
+        <li class=''><span class="mdi mdi-shield-check" style="font-size: 20px; ">  Privacy</span></li>
+      </ul>
+        
+   </div>
+      
       <div id="content1">content1</div>
     </div>
 </body>
